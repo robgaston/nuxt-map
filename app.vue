7b1a4dd7dfa9eb5be344febfcd39df65a34b7e94
Rob Gaston <rgaston@fargeo.com>
2024-08-12 10:25:06 -0700

    cleans up styles, refactors

--- app.vue
+++ app.vue
@@ -2,63 +2,53 @@
 import { onMounted, ref } from 'vue';
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import custom from "./custom-style.json";
+import style from "./mapbox/style.json";
 
 const hoverFeature = ref();
 const legendItems = ref([]);
 const attribution = ref();
+const mapEl = ref();
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoicmdhc3RvbiIsImEiOiJJYTdoRWNJIn0.MN6DrT07IEKXadCU8xpUMg';
 
 onMounted(() => {
   const map = new mapboxgl.Map({
-    "container": "map",
-    "style": "mapbox://styles/rgaston/cltah5jc400kj01raejpg0tkh",
-    "zoom": 9,
-    "center": [
-      -118.3875,
-      34.03756
-    ],
-    "pitch": 40,
+    "container": mapEl.value,
+    "style": style,
     "customAttribution": attribution.value.innerHTML
   });
 
   map.on("load", async () => {
     const neighborhoods = await import("../data/count-by-neighborhoods.json");
-    const style = map.getStyle();
-    style.sources = {
-      ...style.sources,
-      ...custom.sources
-    };
-    style.layers.push(...custom.layers);
-    map.setStyle(style);
+    const sourceName = "neighborhoods-polygons";
+    const layerName = "neighborhoods-fill";
 
-    map.getSource("neighborhoods-polygons")
+    map.getSource(sourceName)
       .setData(neighborhoods);
 
-    map.on("mousemove", "neighborhoods-fill", (event) => {
+    map.on("mousemove", layerName, (event) => {
       hoverFeature.value = event.features[0];
 
       map.removeFeatureState({
-        source: "neighborhoods-polygons"
+        source: sourceName
       });
       map.setFeatureState({
-        source: "neighborhoods-polygons",
+        source: sourceName,
         id: event.features[0].id
       }, {
         hover: true
       });
     });
 
-    map.on("mouseleave", "neighborhoods-fill", (event) => {
+    map.on("mouseleave", layerName, (event) => {
       hoverFeature.value = null;
 
       map.removeFeatureState({
-        source: "neighborhoods-polygons"
+        source: sourceName
       });
     });
 
-    const fillColorStyle = map.getPaintProperty("neighborhoods-fill", "fill-extrusion-color");
+    const fillColorStyle = map.getPaintProperty(layerName, "fill-extrusion-color");
     let fromValue = 0;
     fillColorStyle.splice(0, 2);
 
@@ -82,29 +72,35 @@ onMounted(() => {
 </script>
 
 <template>
-  <header>
-    Cataloged Historic Sites in Los Angeles by Neighborhood
-    <div>Rob Gaston, March 2024</div>
-  </header>
-  <main>
-    <div id="map"></div>
-    <div id="popup" v-if="hoverFeature">
-      <div>{{ hoverFeature.properties.name }}</div>
-      <div>
-        <span>{{ hoverFeature.properties.count }}</span>
-        historic site(s) cataloged
+  <div class="container">
+    <div class="panel">
+      <div class="title">
+        Cataloged Historic Sites in Los Angeles by Neighborhood
+      </div>
+      <div class="subtitle">
+        Rob Gaston, March 2024
       </div>
     </div>
-    <div id="legend">
-      <div># of Sites</div>
-      <div v-for="item in legendItems">
-        <span class="color" v-bind:style="{ backgroundColor: item.color }"></span>
-        &nbsp;
-        <span>{{ item.text }}</span>
+    <div class="content">
+      <div ref="mapEl" class="map"></div>
+      <div class="panel floating top" v-if="hoverFeature">
+        <div>{{ hoverFeature.properties.name }}</div>
+        <div>
+          <span>{{ hoverFeature.properties.count }}</span>
+          historic site(s) cataloged
+        </div>
+      </div>
+      <div class="panel floating bottom" v-show="legendItems.length > 0">
+        <div># of Sites</div>
+        <div v-for="item in legendItems">
+          <span class="color" v-bind:style="{ backgroundColor: item.color }"></span>
+          &nbsp;
+          <span>{{ item.text }}</span>
+        </div>
       </div>
     </div>
-  </main>
-  <span ref="attribution" class="attribution">
+  </div>
+  <div ref="attribution" class="hidden">
     <a href='https://hpla.lacity.org/' target='_blank'>
       Historic Places LA
     </a>
@@ -112,57 +108,54 @@ onMounted(() => {
     <a href='https://geohub.lacity.org/' target='_blank'>
       Los Angeles City GeoHub
     </a>
-  </span>
+  </div>
 </template>
 
 <style scoped>
-main {
-  position: fixed;
-  top: 66px;
-  right: 0;
-  bottom: 0;
-  left: 0;
-}
-
-#map {
+.container {
+  display: flex;
+  flex-flow: column;
   height: 100%;
 }
 
-header {
-  font-weight: bold;
-  background-color: rgb(190, 190, 190);
-  border-bottom: 2px solid black;
+.content {
+  flex-grow: 1;
+  position: relative
 }
 
-header div {
-  font-size: 0.9em;
-  font-weight: normal;
+.map {
+  height: 100%;
 }
 
-.attribution {
+.hidden {
   display: none;
 }
 
-#popup {
-  top: 86px;
-}
-
-#popup,
-#legend {
-  position: fixed;
-  right: 20px;
+.panel {
   border: 1px solid black;
-}
-
-#legend {
-  bottom: 40px;
-}
-
-header,
-#popup,
-#legend {
   background-color: rgb(190, 190, 190);
   padding: 15px;
+}
+
+.floating {
+  position: absolute;
+  right: 20px;
+}
+
+.top {
+  top: 20px;
+}
+
+.bottom {
+  bottom: 50px;
+}
+
+.title {
+  font-weight: bold;
+}
+
+.subtitle {
+  font-size: 0.9em;
 }
 
 .color {
